f0c0c3c9a67253a6d311e3a970f05a94c6fa67a6
Jonathan Cottrell <cottrell.jonathan@gmail.com>
2022-12-13 20:58:55 -0800

    create home page

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,31 +1,16 @@
 import { Location } from "../location";
-import Image from "next/image";
+import postersData from "../data/posters.json";
 
 export default function Main({ location }: { location: Location }) {
-  // FIXME: Get slides from location.
-  const slides = [
-    {
-      img: "/images/ny/ny-home-1.jpg",
-      dest: `/${location}/events`,
-      title: "Event Name",
-    },
-  ];
+  const posters = postersData[location];
 
   return (
     <>
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-2">
-        {slides.map((slide, index) => (
-          <a key={index} className="flex justify-center" href={slide.dest}>
-            <div
-              style={{ width: "100%", height: "100%", position: "relative" }}
-            >
-              <Image
-                fill
-                alt={slide.title}
-                src={slide.img}
-                objectFit="contain"
-              />
-            </div>
+        {posters.map((poster, index) => (
+          <a key={index} className="flex justify-center" href={poster.link}>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img alt={poster.title} src={poster.img} className="w-full" />
           </a>
         ))}
       </div>
